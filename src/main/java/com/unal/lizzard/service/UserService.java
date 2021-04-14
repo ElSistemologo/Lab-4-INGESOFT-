8242aa929c87d2642bc9fe8e29e8821ea0bb37ca
Carlos Jimenez <cajimenezca@unal.edu.co>
2021-04-13 21:02:35 -0500

    configuration lab 5

--- src/main/java/com/unal/lizzard/service/UserService.java
+++ src/main/java/com/unal/lizzard/service/UserService.java
@@ -3,7 +3,9 @@ package com.unal.lizzard.service;
 import com.unal.lizzard.model.User;
 import com.unal.lizzard.web.dto.UserRegistrationDto;
 
-public interface UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
 
     User save(UserRegistrationDto registrationDto);
 
--- src/main/java/com/unal/lizzard/service/UserServiceImpl.java
+++ src/main/java/com/unal/lizzard/service/UserServiceImpl.java
@@ -4,6 +4,9 @@ import com.unal.lizzard.model.Role;
 import com.unal.lizzard.model.User;
 import com.unal.lizzard.repository.UserRepository;
 import com.unal.lizzard.web.dto.UserRegistrationDto;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -11,21 +14,22 @@ import java.util.Arrays;
 @Service
 public class UserServiceImpl implements UserService {
 
-
     private UserRepository userRepository;
 
-    public  UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository) {
         super();
-        this.userRepository =userRepository;
+        this.userRepository = userRepository;
     }
+
     @Override
     public User save(UserRegistrationDto registrationDto) {
-        User user = new User(
-        registrationDto.getFirstName(),
-        registrationDto.getlastName(),
-        registrationDto.getEmail(),
-        registrationDto.getPassword(),
-        Arrays.asList(new Role( "ROLE_USER")));
+        User user = new User(registrationDto.getFirstName(), registrationDto.getlastName(), registrationDto.getEmail(),
+                registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
         return userRepository.save(user);
-        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
